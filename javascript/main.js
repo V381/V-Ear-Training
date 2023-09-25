c8c1059239a5d632fb76ded8cf3a963bf83c7303
Pavle Paunovic <paavlee@yahoo.com>
2023-09-25 18:49:43 +0200

    Basic Logic Added

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+const 
+    $ = document;
+
+
+
+const 
+    play = $.querySelector(".play-button__play"),
+    lowE = $.querySelectorAll(".low-e ul li"),
+    intervals = $.querySelectorAll(".intervals button"),
+    notes = $.querySelector('.notes').children;
+
+function addActionToIntervalButton(button) {
+    button.addEventListener("click", (e) => {
+        console.log(e.target.innerHTML);
+    })
+}
+  
+
+function addActionToPlayButton() {
+    function twoRandomNotes() {
+        console.log("TODO....")
+    }
+    twoRandomNotes();
+;}
+
+intervals.forEach(addActionToIntervalButton)
+play.addEventListener("click", addActionToPlayButton);
+
+
